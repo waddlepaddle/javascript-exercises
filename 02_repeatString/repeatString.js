4b91b0d9ce816371466d36e8bd98dca4a250a820
waddlepaddle <168268571+waddlepaddle@users.noreply.github.com>
2024-08-14 19:20:06 +0800

    Repeat string for set amount of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let string = "";
+    for (let i = 0; i < times; i++) {
+        string += String(word);
+    }
+    return string;
 };
 
 // Do not edit below this line
