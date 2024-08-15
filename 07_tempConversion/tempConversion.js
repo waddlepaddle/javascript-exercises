bd00a8f06a1fa636d0e93e9194054e47768e7b54
waddlepaddle <168268571+waddlepaddle@users.noreply.github.com>
2024-08-15 15:26:51 +0800

    Convert celsius to fahrenheit and vice versa using Math.round or toFixed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  // return parseFloat(((fahrenheit - 32)*(5/9)).toFixed(1));
+  return Math.round(((fahrenheit - 32) * (5 / 9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  // return parseFloat((celsius * (9 / 5) + 32).toFixed(1));
+  return Math.round((celsius * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
