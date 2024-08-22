96c1e207a67b6e88d4c4c4e3cec4bcfaf0d5ab3b
waddlepaddle <168268571+waddlepaddle@users.noreply.github.com>
2024-08-23 00:41:18 +0800

    Complete calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, num) => product * num);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+	for (let i = 1; i <= a; i++) {
+    total *= i;
+  }
+  
+  return total;
 };
 
 // Do not edit below this line
