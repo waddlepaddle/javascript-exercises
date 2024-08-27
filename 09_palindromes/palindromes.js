d690cf4a027af0524a63150113efac70b4d543b4
waddlepaddle <168268571+waddlepaddle@users.noreply.github.com>
2024-08-27 15:12:56 +0800

    Palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (arr) {
 
+    // SHOULD USE FILTER INSTEAD TO FILTER OUT UNKNOWNS!!!!!!! -WE ONLY WANT ALPHANUMERICALSSSSSSSSS
+    const regex = /[\~\!\@\#\$\%\^\&\*\(\)\_\+\,\.\ ]/g;
+
+    arr = arr.toLowerCase();
+    arr = arr.replaceAll(regex,"");
+    arrWord = arr.split("");
+    let palindrome = "";
+    for (let i = (arrWord.length - 1); i >= 0; i--) {
+        palindrome += arrWord[i];
+    }
+    return arr === palindrome;
 };
 
 // Do not edit below this line
