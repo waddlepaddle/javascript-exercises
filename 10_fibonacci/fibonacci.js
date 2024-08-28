f4fd8bb0fc9e6926f898bb76eefd7be6f6675169
waddlepaddle <168268571+waddlepaddle@users.noreply.github.com>
2024-08-29 00:40:40 +0800

    Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    n = +n;
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
 
+    let arr = [1, 1];
+    for (let i = 2; i < n; i++) {
+        arr[i] = arr[i - 2] + arr[i - 1];
+    }
+    return arr[n - 1]
 };
 
 // Do not edit below this line
