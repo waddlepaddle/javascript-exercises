b4a6dc4e59be1bb7e7084f8b77ca0a458495ee1c
waddlepaddle <168268571+waddlepaddle@users.noreply.github.com>
2024-08-29 22:34:13 +0800

    Oldest person exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,42 @@
-const findTheOldest = function() {
+const getAge = function(yearOfBirth, yearOfDeath) {
+    if (!yearOfDeath) yearOfDeath = new Date().getFullYear();
+    return yearOfDeath - yearOfBirth;
+};
 
+const findTheOldest = function(people) {
+    // let age = [];
+    // const currentYear = (new Date()).getFullYear();
+
+    // people.forEach((person) => {
+    //     if (person.yearOfDeath == null) {
+    //         age.push(currentYear - person.yearOfBirth);
+    //     } else {
+    //         age.push(person.yearOfDeath - person.yearOfBirth);
+    //     }
+    // })
+
+    // maxAge = Math.max(...age);
+    // maxAgeIndex = age.indexOf(maxAge);
+    // return people[maxAgeIndex];
+
+    // ------------------------------------------------------------------------------
+    // const oldestToYoungest = people.toSorted((firstPerson, secondPerson) => {
+    //     getAge(secondPerson.yearOfDeath, secondPerson.yearOfBirth) - 
+    //     getAge(firstPerson.yearOfDeath, firstPerson.yearOfBirth)
+    // });
+
+    // return oldestToYoungest[0];
+
+    return people.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        if (currentAge > oldestAge) {
+            return current;
+        } else {
+            return oldest;
+        }
+    });
 };
 
 // Do not edit below this line
