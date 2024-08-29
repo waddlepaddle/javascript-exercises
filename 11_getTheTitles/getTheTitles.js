e5fcb71755c36c030aa9313e5d852a1f01dad9ab
waddlepaddle <168268571+waddlepaddle@users.noreply.github.com>
2024-08-29 13:25:30 +0800

    Array map for objects

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // let titles = [];
+    // books.forEach(book => {
+    //     titles.push(book.title)
+    // });
+    // return titles;
 
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
